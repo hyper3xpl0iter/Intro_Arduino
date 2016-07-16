7b7cb1ee769fe548e27a892602cae06c1174d6a6
git <hyper3xpl0iter@gmail.com>
2016-07-16 15:08:23 -0400

    updates

--- LEDstrip_gazebo/ledstrip_openhab_gazebo.ino
+++ LEDstrip_gazebo/ledstrip_openhab_gazebo.ino
@@ -9,58 +9,71 @@
 //
 /////////////////////////////////////////////////////////////////////
 
-//pin definitions.  must be PWM-capable pins!
-//Channel 01 pin settings (
-const int redPin = 6;
-const int greenPin = 5;
-const int bluePin = 3;
-
-const int BredPin = 11;
-const int BgreenPin = 10;
-const int BbluePin = 9;
+// pin definitions.  must be PWM-capable pins!
+// channel 01 pin definitions
+    #define REDPINONE 6
+    #define GREENPINONE 5
+    #define BLUEPINONE 3
+    
 
 
-//pin definitions for amp02
+// channel 02 pin definitions
+	#define REDPINTWO 11
+    #define GREENPINTWO 10
+    #define BLUEPINTWO 9
+     
+// fadespeed definitoin (make this higher to slow down)   
+	#define FADESPEED 20
 
-//maximum duty cycle to be used on each led for color balancing.  
-//if "white" (R=255, G=255, B=255) doesn't look white, reduce the red, green, or blue max value.
-const int max_red = 255;
-const int max_green = 90;
-const int max_blue = 100;
+// define a mode for the pins
+    void setup() {
+      pinMode(REDPINONE, OUTPUT);
+      pinMode(GREENPINONE, OUTPUT);
+      pinMode(BLUEPINONE, OUTPUT);
+      pinMode(REDPINTWO, OUTPUT);
+      pinMode(GREENPINTWO, OUTPUT);
+      pinMode(BLUEPINTWO, OUTPUT);
+    }
+     
 
-byte colors[3] = {0, 0, 0}; //array to store led brightness values
-byte lineEnding = 0x0A; //10 in decimal, ASCII newline character
-
-void setup(){
-  //set all three of our led pins to output
-  // Amp01 pin mode
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-
-  // Amp02 pin mode
-  pinMode(BredPin, OUTPUT);
-  pinMode(BgreenPin, OUTPUT);
-  pinMode(BbluePin, OUTPUT);
-  
-  //start the Serial connection
-  Serial.begin(115200);
-}
-
-void loop(){
-  //check that at least 3 bytes are available on the Serial port
-  if (Serial.available() > 2){
-     //store data up until lineEnding (0x0A) in the bytesRead array
-     int bytesRead = Serial.readBytesUntil(lineEnding, (char*)colors, 3);
-  }  
-  //set the three PWM pins according to the data read from the Serial port
-  //we also scale the values with map() so that the R, G, and B brightnesses are balanced.
-  analogWrite(redPin, map(colors[0], 0, 255, 0, max_red));
-  analogWrite(greenPin, map(colors[1], 0, 0, 0, max_green));
-  analogWrite(bluePin, map(colors[2], 0, 0, 0, max_blue));
-
-  // Amp02 write config
-  analogWrite(BredPin, map(colors[0], 0, 255, 0, max_red));
-  analogWrite(BgreenPin, map(colors[1], 0, 0, 0, max_green));
-  analogWrite(BbluePin, map(colors[2], 0, 0, 0, max_blue));
-}
+ // make loop to cycle through colors and for statments defining colors to fade too
+    void loop() {
+      int r, g, b;
+     
+      // fade from blue to violet
+      for (r = 0; r < 256; r++) { 
+        analogWrite(REDPINONE, r);
+        analogWrite(REDPINTWO, r);
+        delay(FADESPEED);
+      } 
+      // fade from violet to red
+      for (b = 255; b > 0; b--) { 
+        analogWrite(BLUEPINONE, b);
+        analogWrite(BLUEPINTWO, b);
+        delay(FADESPEED);
+      } 
+      // fade from red to yellow
+      for (g = 0; g < 256; g++) { 
+        analogWrite(GREENPINONE, g);
+        analogWrite(GREENPINTWO, g);
+        delay(FADESPEED);
+      } 
+      // fade from yellow to green
+      for (r = 255; r > 0; r--) { 
+        analogWrite(REDPINONE, r);
+        analogWrite(REDPINTWO, r);
+        delay(FADESPEED);
+      } 
+      // fade from green to teal
+      for (b = 0; b < 256; b++) { 
+        analogWrite(BLUEPINONE, b);
+        analogWrite(BLUEPINTWO, b);
+        delay(FADESPEED);
+      } 
+      // fade from teal to blue
+      for (g = 255; g > 0; g--) { 
+        analogWrite(GREENPINONE, g);
+        analogWrite(GREENPINTWO, g);
+        delay(FADESPEED);
+      } 
+    }
